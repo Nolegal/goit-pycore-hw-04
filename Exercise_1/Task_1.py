6493fb2ff8873106ebd3b8b9da5b69af71129fcd
Nolegal <nolegal@ukr.net>
2024-04-22 22:23:43 +0300

    stage_2

--- Exercise_1/Task_1.py
+++ Exercise_1/Task_1.py
@@ -1,7 +1,5 @@
 
 def total_salary(path):
-    with open("text.txt", "r", encoding='utf-8') as path:
-      
+   with open('C:\\Users\\Aleksandr\\Desktop\\Homework\\Topic_6\\Exercise_1\\text.txt', 'r', encoding='utf-8') as path:
     # lines=[el.split(",")for el in path.readlines()]
-       
-     print(path)
+     print(path.read())
